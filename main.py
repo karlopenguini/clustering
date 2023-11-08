d8a9df4b6df538a129e734cf9760bbb936e16eac
Karlo Miguel Palisoc <karobpalisoc@gmail.com>
2023-11-08 18:07:36 +0800

    qasd

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import pickle
+
+app = Flask(__name__)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    model = pickle.load(open('model.sav','rb'))
+    data = request.json  # Get input data from the request
+    dict = {
+        0:"Home Maintenance Enthusiasts",
+        1:"Family-Centric Homeowners",
+        2:"Active & Independent Residents",
+        3:"Young & Energetic Urban Dwellers",
+        4:"Family-Centric Budgeters",
+        5:"Elite & Well-Established Homeowners",
+    }
+    return dict[model.predict([data['payload']]).reshape(-1)[0]]
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
